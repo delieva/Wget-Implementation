b7e9a3f315821a5c31175096dc81f1ddb83297f9
delieva <delievaalexandra@gmail.com>
2018-12-18 22:24:10 +0200

    Information about downloading

--- public/main.js
+++ public/main.js
@@ -44,10 +44,16 @@ download.addEventListener('click', (e) => {
 					let month = temp_date.getMonth() + 1;
 					let year = temp_date.getYear() - 100 + 2000;
 					document.getElementById('result').innerHTML = "\""+res.name+"\"" + " downloaded. Size: " + res.size + 'Kb';
-					document.getElementById('timeDate').innerHTML = "date: " + day + "." + month + "." + year + "                    time: " + temp_date.getHours() + ":" + temp_date.getMinutes() + ":" + temp_date.getSeconds()
+					document.getElementById('timeDate').innerHTML = "date: " + day + "." + month + "." + year + "   time: " + temp_date.getHours() + ":" + temp_date.getMinutes() + ":" + temp_date.getSeconds()
 				}
 				else{
+					
+					let temp_date = new Date();
+					let day = temp_date.getDate();
+					let month = temp_date.getMonth() + 1;
+					let year = temp_date.getYear() - 100 + 2000;
 					document.getElementById('result').innerHTML = "\""+res.name+"\"" + " downloaded"
+					document.getElementById('timeDate').innerHTML = "date: " + day + "." + month + "." + year + "   time: " + temp_date.getHours() + ":" + temp_date.getMinutes() + ":" + temp_date.getSeconds()
 				}
 			}
 			else {
